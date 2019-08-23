f2f17d23354da9aab69df7f361519fb94e9273d6
Faye Skeen <fskeen@gmail.com>
2019-08-23 08:54:34 -0700

    Built login, priv Route, axiosWithAuth. Login functional.

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,13 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from 'axios'
 
-const Login = () => {
+const Login = (props) => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const [creds, setCreds] = useState({
+    username: '',
+    password: ''
+  })
+
+  const handleChange = e => {
+    return setCreds({
+      ...creds,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const routeToBubbles = () => {
+    props.history.push('/bubbles')
+  }
+
+  const login = e => {
+    e.preventDefault();
+    axios.post('http://localhost:5000/api/login', { username: 'Lambda School', password: 'i<3Lambd4' })
+      .then(res => {
+        console.log(res)
+        console.log("creds", creds)
+        localStorage.setItem('token', res.data.payload)
+        routeToBubbles()
+      })
+      .catch(err => {
+        console.log("creds", creds)
+        console.log("Error getting stuff from server!", err)})
+  }
+
   return (
-    <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
-    </>
+    <form onSubmit={login}>
+      <div>
+        <label htmlFor="username">Enter username</label>
+        <input
+            type="text"
+            id="username"
+            name="username"
+            value={creds.username}
+            onChange={handleChange} />
+      </div>
+
+      <div>
+          <label htmlFor="password">Enter password</label>
+          <input
+              type="password"
+              id="password"
+              name="password"
+              value={creds.password}
+              onChange={handleChange} />
+      </div>
+
+      <button type="submit">Submit!</button>
+    </form>
   );
 };
 
